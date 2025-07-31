b4e369d6662086265b09b3731c6557af12716527
ZachJ1998 <35534804+ZachJ1998@users.noreply.github.com>
2025-07-31 14:50:26 -0500

    Macros added

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,20 @@
 #![no_std] // don't link the Rust standard library
 #![no_main] // disable all Rust-level entry points
+mod vga_buffer;
 
 use core::panic::PanicInfo;
 
 /// This function is called on panic.
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
+fn panic(info: &PanicInfo) -> ! {
+    println!("{info}");
     loop {}
 }
 
-mod vga_buffer;
 #[unsafe(no_mangle)]
 pub extern "C" fn _start() -> ! {
-    vga_buffer::print_something();
-
+    println!("Hello World{}", "!");
+    panic!("Some panic message");
+    
     loop {}
 }
-
